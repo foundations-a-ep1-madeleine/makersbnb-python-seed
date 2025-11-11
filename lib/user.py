2685d41424eff6a000533d1c9d36bcb1fd61af02
Stallper <nazar.jaman@gmail.com>
2025-11-11 16:33:05 +0000

    feat(db): Update users table shema and test 301

--- lib/user.py
+++ lib/user.py
@@ -1,13 +1,14 @@
 class User:
 
-    def __init__(self, id, name, email, password):
+    def __init__(self, id, name, email, password_hash, created_at=None):
         self.id = id
         self.name = name 
         self.email = email
-        self.password = password
+        self.password_hash = password_hash
+        self.created_at = created_at
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
     
     def __repr__(self):
-        return f"User({self.id}, {self.name}, {self.email}, {self.password})"
+        return f"User({self.id}, {self.name}, {self.email}, {self.password_hash}, {self.created_at})"
--- lib/user_repository.py
+++ lib/user_repository.py
@@ -1,55 +1,37 @@
-from lib import user
+from lib.user import User
+
 class UserRepository:
-    def init(self, connection):
-    
+    def __init__(self, connection):
         self._connection = connection
 
     def all(self):
-        
-
         rows = self._connection.execute('SELECT * FROM users')
         users = []
         for row in rows:
-            user = user(row["id"], row["name"], row["email"], row["password"])
+            user = User(row["id"], row["name"], row["email"], row["password_hash"], row["created_at"])
             users.append(user)
         return users
 
     def find(self, user_id):
-        
-        rows = self._connection.execute(
-            'SELECT * FROM users WHERE id = %s', [user_id])
-        if not rows:
-            return None
+        rows = self._connection.execute('SELECT * FROM users WHERE id = %s', [user_id])
+        if not rows: return None
         row = rows[0]
-        return user(row["id"], row["name"], row["email"], row["password"])
+        return User(row["id"], row["name"], row["email"], row["password_hash"], row["created_at"])
 
     def find_by_email(self, email):
         rows = self._connection.execute('SELECT * FROM users WHERE email = %s', [email])
-        if not rows:
-            return None
+        if not rows: return None
         row = rows[0]
-        return user(row["id"], row["name"], row["email"], row["password"])
+        return User(row["id"], row["name"], row["email"], row["password_hash"], row["created_at"])
 
     def create(self, user):
-        
         self._connection.execute(
-            'INSERT INTO users (name, email, password) VALUES (%s, %s, %s)',
-            [user.name, user.email, user.password]
-        )
-        return None
-
-    def update(self, user):
-      
-        self._connection.execute(
-            'UPDATE users SET name = %s, email = %s, password = %s WHERE id = %s',
-            [user.name, user.email, user.password, user.id]
+            'INSERT INTO users (name, email, password_hash) VALUES (%s, %s, %s)',
+            [user.name, user.email, user.password_hash]
         )
         return None
 
     def delete(self, user_id):
-     
-        self._connection.execute(
-            'DELETE FROM users WHERE id = %s', [user_id]
-        )
+        self._connection.execute('DELETE FROM users WHERE id = %s', [user_id])
         return None
 
