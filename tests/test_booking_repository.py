6a4d18ccf742f29f27aa3a1687c488b6b6dbc34c
Isaac Madgwick <isaacmadgwick@gmail.com>
2025-11-11 12:04:44 +0000

    Updated seed file and finished booking repo

--- tests/test_booking_repository.py
+++ tests/test_booking_repository.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 
 from lib.booking import Booking
 from lib.booking_repository import BookingRepository
@@ -10,14 +10,36 @@ def test_get_all_bookings(db_connection):
     bookings = repository.all()
 
     assert bookings == [
-        Booking(1, datetime(2025,11,05)),
-
+        Booking(1, date(2025,11,5), False, 1, 2),
+        Booking(2, date(2025,11,10), False, 2, 3),
+        Booking(3, date(2025,12,12), True, 3, 4),
+        Booking(4, date(2025,11,18), True, 4, 5),
+        Booking(5, date(2025,12,5), False, 5, 6),
+        Booking(6, date(2025,11,12), True, 6, 1),
     ]
 
     pass
 
+def test_get_booking(db_connection):
+    db_connection.seed("seeds/makersbnb.sql")
+    repository = BookingRepository(db_connection)
+
+    booking = repository.get_by_id(2)
+
+    assert booking == Booking(2, date(2025,11,10), False, 2, 3)
+
 def test_get_bookings_by_renter_id(db_connection):
-    pass
+    db_connection.seed("seeds/makersbnb.sql")
+    repository = BookingRepository(db_connection)
+
+    booking = repository.get_by_renter(4)
+
+    assert booking == [Booking(3, date(2025,12,12), True, 3, 4)]
 
 def test_get_bookings_by_space_id(db_connection):
-    pass
+    db_connection.seed("seeds/makersbnb.sql")
+    repository = BookingRepository(db_connection)
+
+    booking = repository.get_by_space(4)
+
+    assert booking == [Booking(4, date(2025,11,18), True, 4, 5)]
