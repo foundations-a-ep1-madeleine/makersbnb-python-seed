255dbde63660448f90ff0d1e3d58b9853b4d16b5
margotbourne <margotbourne@icloud.com>
2025-11-11 16:02:54 +0000

    TDD Spaces and SpaceRepo

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
 import os
 from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
+from lib.space_repository import SpaceRepository
+from lib.space import Space
+
 
 # Create a new Flask app
 app = Flask(__name__)
 
 # == Your Routes Here ==
+
+@app.route('/spaces', methods=['GET'])
+def get_space():
+    connection = get_flask_database_connection(app)
+    space_repo = SpaceRepository(connection)
+    spaces = space_repo.all()
+    return render_template('spaces.html', spaces=spaces)
+    
+
+
+@app.route('/spaces', methods=['POST'])
+def create_space():
+    connection = get_flask_database_connection(app)
+    repository = SpaceRepository(connection)
+    space = Space(None, request.form['name'], request.form['description'], request.form['price', request.form['user_id']])
+    space = repository.create(space)
+    return "Space added successfully"
+
+@app.route('/spaces/<id>', methods=['GET'])
+def get_space_by_user_id(id):
+        connection = get_flask_database_connection(app)
+        repository = SpaceRepository(connection)
+        space = repository.find(id)
+        return render_template('single_space_id.html', space=space)
+
 
 # GET /index
 # Returns the homepage
---
+++ lib/space_repository.py
@@ -0,0 +1,30 @@
+from lib.space import *
+
+class SpaceRepository:
+
+    def __init__(self, connection):
+        self._connection = connection
+
+    def all(self):
+        rows = self._connection.execute('SELECT * from spaces')
+        spaces = []
+        for row in rows:
+            item = Space(row["id"], row["name"], row["description"], row["price"], row["user_id"])
+            spaces.append(item)
+        return spaces
+
+    def find(self, space_id):
+        rows = self._connection.execute(
+            'SELECT * from spaces WHERE id = %s', [space_id])
+        row = rows[0]
+        return Space(row["id"], row["name"], row["description"], row["price"], row["user_id"])
+    
+    def create(self, space):
+        self._connection.execute('INSERT INTO spaces (name, description, price, user_id) VALUES (%s, %s, %s, %s)', [
+                                 space.name, space.description, space.price, space.user_id ])
+        return None
+
+    def delete(self, space_id):
+        self._connection.execute(
+            'DELETE FROM spaces WHERE id = %s', [user_id])
+        return None
