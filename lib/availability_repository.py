c1a7091280a23f186807663e7c47406bd73329b7
sabiajey <jeysabia@gmail.com>
2025-11-11 15:47:03 +0000

    Availability repository files complete

---
+++ lib/availability_repository.py
@@ -0,0 +1,52 @@
+from lib.availability import *
+
+class AvailabilityRepository():
+    
+    def __init__(self, connection):
+        self._connection = connection
+
+    def all(self):
+        rows = self._connection.execute('SELECT * FROM availabilities ORDER BY id')
+        availabilities = []
+        for row in rows:
+            item = Availability(row["id"],row["start_date"],row["end_date"],row["space_id"])
+            availabilities.append(item)
+        return availabilities
+
+
+    def find_by_id(self,id):
+        rows = self._connection.execute('SELECT * FROM availabilities WHERE id = %s ', [id])
+        row = rows[0]
+        availability = Availability(row["id"],row["start_date"],row["end_date"],row["space_id"])
+        return availability
+
+    
+    def find_by_start_date(self,start_date):
+        rows = self._connection.execute('SELECT * FROM availabilities WHERE start_date = %s ', [start_date])
+        availabilities = []
+        for row in rows:
+            item = Availability(row["id"],row["start_date"],row["end_date"],row["space_id"])
+            availabilities.append(item)
+        return availabilities
+    
+
+    def find_by_space_id(self,space_id):
+        rows = self._connection.execute('SELECT * FROM availabilities WHERE space_id = %s ', [space_id])
+        availabilities = []
+        for row in rows:
+            item = Availability(row["id"],row["start_date"],row["end_date"],row["space_id"])
+            availabilities.append(item)
+        
+        return availabilities
+
+    def create(self,availability):
+        self._connection.execute('INSERT INTO availabilities(start_date,end_date, space_id) VALUES(%s,%s,%s)',[availability.start_date, availability.end_date, availability.space_id])
+        return None
+
+    def update(self,availability):
+        self._connection.execute('UPDATE availabilities SET start_date = %s, end_date = %s , space_id = %s WHERE id = %s',[availability.start_date, availability.end_date, availability.space_id, availability.id])
+        return None
+
+    def delete(self,id):
+        self._connection.execute('DELETE FROM availabilities WHERE id = %s',[id])
+        return None
---
+++ tests/test_availability_repository.py
@@ -0,0 +1,101 @@
+from lib.availability_repository import *
+from lib.availability import *
+from datetime import date
+
+
+def test_get_all_records(db_connection):
+    db_connection.seed("seeds/makersbnb.sql")
+    repository = AvailabilityRepository(db_connection)
+
+    availability = repository.all()
+   
+    assert availability == [
+        Availability(1,date(2025,11,1), date(2025,11,15), 1),
+        Availability(2,date(2025,12,1), date(2025,12,20), 1),
+        Availability(3,date(2025,11,5), date(2025,11,25), 2),
+        Availability(4,date(2025,12,10), date(2025,12,31), 3),
+        Availability(5,date(2025,11,15), date(2025,11,30), 4),
+        Availability(6,date(2025,12,1), date(2025,12,15), 5),
+        Availability(7,date(2025,11,10), date(2025,11,20), 6)
+    ]
+
+
+def test_get_single_record_by_id(db_connection):
+    db_connection.seed("seeds/makersbnb.sql")
+    repository = AvailabilityRepository(db_connection)
+
+    availability = repository.find_by_id(3)
+    assert availability == Availability(3,date(2025,11,5), date(2025,11,25), 2)
+
+   
+
+def test_get_single_record_by_start_date(db_connection):
+    db_connection.seed("seeds/makersbnb.sql")
+    repository = AvailabilityRepository(db_connection)
+
+    availability = repository.find_by_start_date(date(2025,12,1))
+    assert availability == [
+    Availability(2, date(2025,12,1), date(2025,12,20), 1),
+    Availability(6, date(2025,12,1), date(2025,12,15), 5)
+]
+
+def test_get_single_record_by_space_id(db_connection):
+    db_connection.seed("seeds/makersbnb.sql")
+    repository = AvailabilityRepository(db_connection)
+
+    availability = repository.find_by_space_id(5)
+    assert availability ==  [Availability(6,date(2025,12,1), date(2025,12,15), 5)]
+
+
+
+def test_create_record(db_connection):
+    db_connection.seed("seeds/makersbnb.sql")
+    repository = AvailabilityRepository(db_connection)
+
+    repository.create(Availability(None,date(2025,1,1), date(2025,10,13),6))
+    availability = repository.all()
+    assert availability == [
+        Availability(1,date(2025,11,1), date(2025,11,15), 1),
+        Availability(2,date(2025,12,1), date(2025,12,20), 1),
+        Availability(3,date(2025,11,5), date(2025,11,25), 2),
+        Availability(4,date(2025,12,10), date(2025,12,31), 3),
+        Availability(5,date(2025,11,15), date(2025,11,30), 4),
+        Availability(6,date(2025,12,1), date(2025,12,15), 5),
+        Availability(7,date(2025,11,10), date(2025,11,20), 6),
+        Availability(8,date(2025,1,1), date(2025,10,13), 6)
+    ]
+
+def test_update_record(db_connection):
+    db_connection.seed("seeds/makersbnb.sql")
+    repository = AvailabilityRepository(db_connection)
+
+    repository.update(Availability(2,date(2025,1,1), date(2025,10,13),6))
+    availability = repository.all()
+    assert availability == [
+        Availability(1,date(2025,11,1), date(2025,11,15), 1),
+        Availability(2,date(2025,1,1), date(2025,10,13),6),
+        Availability(3,date(2025,11,5), date(2025,11,25), 2),
+        Availability(4,date(2025,12,10), date(2025,12,31), 3),
+        Availability(5,date(2025,11,15), date(2025,11,30), 4),
+        Availability(6,date(2025,12,1), date(2025,12,15), 5),
+        Availability(7,date(2025,11,10), date(2025,11,20), 6)
+    ]
+
+
+def test_delete_record(db_connection):
+    db_connection.seed("seeds/makersbnb.sql")
+    repository = AvailabilityRepository(db_connection)
+
+    repository.delete(2)
+    availability = repository.all()
+    assert availability == [
+        Availability(1,date(2025,11,1), date(2025,11,15), 1),
+        Availability(3,date(2025,11,5), date(2025,11,25), 2),
+        Availability(4,date(2025,12,10), date(2025,12,31), 3),
+        Availability(5,date(2025,11,15), date(2025,11,30), 4),
+        Availability(6,date(2025,12,1), date(2025,12,15), 5),
+        Availability(7,date(2025,11,10), date(2025,11,20), 6)
+    ]
+
+
+
