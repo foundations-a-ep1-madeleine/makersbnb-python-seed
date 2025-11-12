1f6c166eb4a1e0e43cb65da3899a4c5fc46c7ea3
Isaac Madgwick <isaacmadgwick@gmail.com>
2025-11-12 13:16:32 +0000

    Added basic authentication functions

--- app.py
+++ app.py
@@ -1,18 +1,58 @@
 import os
+import jwt
+import bcrypt
+from functools import wraps
 from flask import Flask, request, render_template, jsonify
 from lib.database_connection import get_flask_database_connection
 from lib.availability_repository import AvailabilityRepository
 from lib.availability import Availability
 from lib.space_repository import SpaceRepository
+from lib.user import User
+from lib.user_repository import UserRepository
 from lib.space import Space
 from lib.date_serialization import string_to_date
 
-
 # Create a new Flask app
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "wow_so_secret"
 
-# == Your Routes Here ==
+# User Authentication #
 
+# Returns a salted, hashed password
+def hash_password(password):
+    password_bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password_bytes, salt)
+
+# Takes in a plain text password and a hashed password and returns a boolean
+# depending on if they are the same
+def compare_password_hash(entered_password, hashed_password):
+    password_bytes = entered_password.encode('utf-8')
+    return bcrypt.checkpw(password_bytes, hashed_password)
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.cookies.get('jwt_token')
+
+        if not token:
+            return jsonify({'message': 'Token missing'}), 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            connection = get_flask_database_connection()
+            repo = UserRepository(connection)
+
+            user = repo.find(data.user_id)
+            
+        except:
+            return jsonify({'message': 'Token invalid'}), 401
+
+        return f(user, *args, **kwargs)
+
+    return decorated
+
+# Routes #
 @app.route('/spaces', methods=['GET'])
 def get_space():
     connection = get_flask_database_connection(app)
@@ -64,6 +104,7 @@ def create_space_availability():
     repository = AvailabilityRepository(connection)
     repository.create(Availability(None, string_to_date(request.form['start_date']), string_to_date(request.form['end_date']), request.form['space_id']))
     return "Availability added"
+
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
