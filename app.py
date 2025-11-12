e7b2b1aa84948d63294aee91720efce97714e094
Isaac Madgwick <isaacmadgwick@gmail.com>
2025-11-12 21:31:03 +0000

    Added home direction and changed get_index to redirect to /spaces

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import jwt
 import bcrypt
 from functools import wraps
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, url_for, redirect
 from lib.database_connection import get_flask_database_connection
 from lib.availability_repository import AvailabilityRepository
 from lib.availability import Availability
@@ -139,10 +139,11 @@ def debug_db_name():
     
 @app.route('/index', methods=['GET'])
 def get_index():
-    connection = get_flask_database_connection(app)
-    space_repo = SpaceRepository(connection)
-    spaces = space_repo.all()
-    return render_template('index.html', spaces=spaces)
+    return redirect(url_for('get_space'))
+
+@app.route('/', methods=['GET'])
+def get_home():
+    return redirect(url_for('get_space'))
 
 @app.route('/spaces/<int:id>/availability', methods=['GET'])
 def get_space_availability(id):
